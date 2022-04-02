ab3668c89772ca47ff594a9c05d9abcf6b02ab33
Troy Coll <troy.coll@gmail.com>
2022-04-02 08:13:00 -0400

    messages

--- db/migrate/20220323144254_create_dailies.rb
+++ db/migrate/20220323144254_create_dailies.rb
@@ -1,8 +1,8 @@
 class CreateDailies < ActiveRecord::Migration[6.0]
   def change
     create_table :dailies do |t|
-      t.date :sent_date
       t.string :message
+      t.date :sent_date
       
       t.timestamps
     end
