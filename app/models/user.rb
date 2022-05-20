ff3a36ab32e7dcf66c679dd3e83a0181f97b2707
Troy Coll <troy.coll@gmail.com>
2022-05-20 19:52:51 -0400

    more linting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,21 @@
+# frozen_string_literal: true
+
+# class EmailValidator validates user emails before creating a new user
 class EmailValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-        unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-            record.errors.add attribute, (options[:message] || 'is not an email' )
-        end
-    end
+  EMAIL_PATTERN = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
+
+  def validate_each(record, attribute, value)
+    record.errors.add attribute, (options[:message] || 'is not an email') if value =~ EMAIL_PATTERN
+  end
 end
 
+# class User models users who subscribe
 class User < ApplicationRecord
-    validates :email, presence: true, email: true 
+  validates :email, presence: true, email: true
 
-    def list_subscribers
-      subscribers = User.where(verified: true, unsubscribed_date: nil)
-      return subscribers
-    end
+  def list_subscribers
+    User.where(verified: true, unsubscribed_date: nil)
+  end
 
-    def validate_captcha
-      
-    end
+  def validate_captcha; end
 end
