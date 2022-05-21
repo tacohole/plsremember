fc96d1ab30c1b59f5285a31d07dbe60bd344bdba
Troy Coll <troy.coll@gmail.com>
2022-05-21 10:05:48 -0400

    about

---
+++ app/javascript/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="vw-100 vh-100 primary-color d-flex justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+        <div className="container secondary-color">
+            <h1 className="display-4">Please Remember</h1>
+            <p>
+                A joyful reminder sent to your inbox once daily
+            </p>
+            <hr className="my-4"/>
+             <div>
+               <p>
+                   here's what it's about
+               </p>
+            </div>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default About
