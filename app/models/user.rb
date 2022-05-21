4d3715248da851e860f672f56fc945cf6af19a77
Troy Coll <troy.coll@gmail.com>
2022-05-20 20:27:58 -0400

    recaptcha verifier

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'net/http'
+
 # class EmailValidator validates user emails before creating a new user
 class EmailValidator < ActiveModel::EachValidator
   EMAIL_PATTERN = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
@@ -13,9 +15,21 @@ end
 class User < ApplicationRecord
   validates :email, presence: true, email: true
 
+  GOOGLE_URL = URI('https://www.google.com/recaptcha/api/siteverify')
+
   def list_subscribers
     User.where(verified: true, unsubscribed_date: nil)
   end
 
-  def validate_captcha; end
+  def validate_captcha
+    https = Net::HTTP.new(GOOGLE_URL.host, GOOGLE_URL.port)
+    https.use_ssl = true
+    verify_request = Net::HTTP::Post.new(uri.path)
+    verify_request.set_form_data(
+      'secret' => ENV['REACT_APP_SECRET_KEY'],
+      'response' => params['g-recaptcha-response']
+    )
+    googleanswer = https.request(verify_request)
+    JSON.parse(googleanswer.body).dig['success']
+  end
 end
