16f1d31aa067d9b30f7e3b04bbe18272284ff39a
Troy Coll <troy.coll@gmail.com>
2022-05-21 21:15:07 -0400

    about

--- app/javascript/components/About.jsx
+++ app/javascript/components/About.jsx
@@ -12,7 +12,25 @@ const About = () => {
             <hr className="my-4"/>
              <div>
                <p>
-                   here's what it's about
+                  ## what is please remember? ##
+                  it's a website where you give me your email address, and i send you a little tweet-length
+                  message to that email once a day. i won't send you anything else, i won't ask you for any money,
+                  i won't give your email address to anyone, and i'll never put an ad on this site or in an email. 
+                  
+                  ## why did you do this? ##
+                  i was inspired by Matthew Ogle's <a href='https://tinyletter.com/pome'>pome</a>, a tinyletter that delivers a poetry
+                  snippet every day. instead of signing up for tinyletter i wanted to roll my own. my intention
+                  is to add a few more ounces of joy to the day of anybody who signs up.             
+
+                  ## where do these messages come from? ##
+                  wherever. some of them are from a fortune cookie database, some of them are from a list of affirmations i found, 
+                  some of them are from my favorite books, movies, or poems. none of them are mine. i don't attribute them 
+                  because for me, the lack of context broadens their potential meaning. if you want to know the source of 
+                  a particular message, you can email me. 
+
+                  ## who are you? ##
+                  please remember is a project by Troy Coll. he's an aspiring software engineer based in Raleigh,
+                  North Carolina. you can contact him at troy@plsremember.com. 
                </p>
             </div>
         </div>
