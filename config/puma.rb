f4777efc39d01ce72214342cc373e1904f6877b9
Troy Coll <troy.coll@gmail.com>
2022-06-05 09:20:25 -0400

    puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# require 'dotenv'
+require 'dotenv'
 
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
