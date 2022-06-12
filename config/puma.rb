fab4a217465c048d75f40026a07a5335e4849e16
Troy Coll <troy.coll@gmail.com>
2022-06-12 09:27:22 -0400

    copy paste config

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
 preload_app!
 
-rackup DefaultRackup
-port ENV['PORT'] || 3000
-environment ENV['RACK_ENV'] || 'development'
+rackup      DefaultRackup
+port        ENV.fetch('PORT') { 3000 }
+environment ENV.fetch('RAILS_ENV') { 'development' }
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection
