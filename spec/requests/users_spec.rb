d26464866ed030b45aa037c13f2aa3f743fccb5f
Troy Coll <troy.coll@gmail.com>
2022-06-18 07:35:44 -0400

    formatting

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,8 +2,8 @@
 
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
     pending "add some examples (or delete) #{__FILE__}"
   end
   # it won't send to a user that already exists
