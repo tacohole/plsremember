21829e6fa2e9f36328dbd73500cb22dac849b99f
Troy Coll <troy.coll@gmail.com>
2022-06-24 17:36:09 -0400

    styling and normalization

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # TO-DO
 
-- style emails
 - buy custom domain
 - setup email on custom domains
-- write some tests
 - link to github
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@
 
 # class UserMailer sends the verification and unsubscribe emails
 class UserMailer < ApplicationMailer
-  default from: 'pleaserememberdevtest@gmail.com'
+  default from: 'troy@pleaseremember.com'
 
   def verification_email
     @user = params[:user]
     @verify_url = "#{ENV['HOST']}/verify/#{@user.code}"
-    mail(to: @user.email, subject: 'pls remember verification email')
+    mail(to: @user.email, subject: 'Please Remember verification email')
   end
 end
